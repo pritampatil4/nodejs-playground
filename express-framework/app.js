233046bce4b491d6d6b01fe394ac69b626cec87a
Pritam Patil <ppritam821@gmail.com>
2021-02-04 17:20:00 +0100

    express route params

--- express-framework/app.js
+++ express-framework/app.js
@@ -9,6 +9,10 @@ app.get("/contact", function (req, res) {
   res.send("This is Contact Page");
 });
 
+app.get('/profile/:id', function(req, res) {
+  res.send('You requested to see the profile of id '  +  req.params.id);
+});
+
 app.listen(3000, function () {
   console.log("Listening on port-3000");
 });
