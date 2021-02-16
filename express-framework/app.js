39d8b94f5ca8ce10414091ae1d65dbbe12fc048c
Pritam Patil <ppritam821@gmail.com>
2021-02-16 16:41:04 +0100

    handling POST requests

--- express-framework/app.js
+++ express-framework/app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
@@ -10,6 +11,10 @@ app.get("/", function (req, res) {
 
 app.get("/contact", function (req, res) {
   res.render("contact", { qs: req.query });
+});
+
+app.post("/contact", urlencodedParser, function (req, res) {
+  res.render("contact-success", { formData: req.body });
 });
 
 app.get("/profile/:name", function (req, res) {
